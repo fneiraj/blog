5df28f09c5ee3f1621c7009ee0764fa4b2332ff7
Fernando Neira <fe.neiraj@gmail.com>
2024-02-16 13:06:54 -0300

    testing husky

--- src/config.ts
+++ src/config.ts
@@ -16,29 +16,34 @@ export const LOCALE = {
 } as const;
 
 export const HEADER_NAV_LINKS = [
-  { href: '/', title: 'Inicio' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/snippets', title: 'Snippets' },
-  { href: '/projects', title: 'Proyectos' },
+  {
+    href: "/",
+    title: "Inicio",
+  },
+  {
+    href: "/blog",
+    title: "Blog",
+  },
+  { href: "/snippets", title: "Snippets" },
+  { href: "/projects", title: "Proyectos" },
   //{ href: '/about-me', title: 'Sobre mi' },
-]
+];
 
 export const FOOTER_LINKS = [
-  { iconName: 'email', href: 'mailto:hi@fneira.dev' },
-  { iconName: 'github', href: 'https://github.com/fneiraj' },
-  { iconName: 'linkedin', href: 'https://www.linkedin.com/in/fneiraj' },
-
-]
+  { iconName: "email", href: "mailto:hi@fneira.dev" },
+  { iconName: "github", href: "https://github.com/fneiraj" },
+  { iconName: "linkedin", href: "https://www.linkedin.com/in/fneiraj" },
+];
 
 export const GISCUS = {
-  id: 'comments',
-  repo: 'fneiraj/blog',
-  repoId: 'R_kgDOIVcPIg',
-  category: 'General',
-  categoryId: 'DIC_kwDOIVcPIs4Cc8s7',
-  mapping: 'pathname',
-  reactionsEnabled: '1',
-  emitMetadata: '0',
-  inputPosition: 'bottom',
-  loading: 'lazy'
+  id: "comments",
+  repo: "fneiraj/blog",
+  repoId: "R_kgDOIVcPIg",
+  category: "General",
+  categoryId: "DIC_kwDOIVcPIs4Cc8s7",
+  mapping: "pathname",
+  reactionsEnabled: "1",
+  emitMetadata: "0",
+  inputPosition: "bottom",
+  loading: "lazy",
 };
