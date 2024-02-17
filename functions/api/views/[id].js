b9b22b885de91270d5b62e09138473a1bf287661
Fernando Neira <fe.neiraj@gmail.com>
2024-02-17 12:21:42 -0300

    cf-page-functions: test

--- functions/api/views/[id].js
+++ functions/api/views/[id].js
@@ -12,7 +12,7 @@ export async function onRequestGet(context) {
   const post = await context.env.PAGE_VIEW_RECORDS?.get("post:" + id);
 
   if (!post) {
-    await context.env.PAGE_VIEW_RECORDS.put("post:" + id, { views: 1 });
+    await context.env.PAGE_VIEW_RECORDS.put("post:" + id, JSON.stringify({ views: 1 }));
     return Response.json({ id, view: 1 });
   }
 
