1a846a5d22e2223cc28120e550b8a748dfa43697
Fernando Neira <fe.neiraj@gmail.com>
2024-02-17 12:10:27 -0300

    cf-page-functions: test

--- functions/api/views/[id].js
+++ functions/api/views/[id].js
@@ -1,15 +1,16 @@
-export function onRequestGet(context) {
+export async function onRequestGet(context) {
   const id = context.params.id;
 
   if (!id) {
     return new Response("Not found", { status: 404 });
   }
 
-  /* const post = posts.find(post => post.id === Number(id))
+  const post = JSON.parse(await context.env.PAGE_VIEW_RECORDS.get("post:" + id));
 
-    if (!post) {
-        return new Response('Not found', { status: 404 })
-    }*/
+  if (!post) {
+    const post = await context.env.PAGE_VIEW_RECORDS.put("post:" + id, { views: 1 });
+    return Response.json({ id, view: 1 });
+  }
 
-  return Response.json({ id, view: 12 });
+  return Response.json({ id, view: post.views });
 }
