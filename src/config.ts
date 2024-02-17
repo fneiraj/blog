e5fae8d78d22fc57e54989cf9cb83b2054d73bf7
Fernando Neira <fe.neiraj@gmail.com>
2024-02-17 00:38:39 -0300

    improvements and add stackoverflow icon

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,10 @@
 export const SITE = {
   website: "https://fneira.dev",
   author: "Fernando Neira",
-  desc: "Personal blog",
+  desc: "fneira.dev - Personal blog",
   repo: "https://github.com/fneiraj/blog",
   title: "fneira.dev",
   ogImage: "astropaper-og.jpg",
-  lightAndDarkMode: true,
   postPerPage: 3,
   scheduledPostMargin: 15 * 60 * 1000,
 };
@@ -32,18 +31,24 @@ export const HEADER_NAV_LINKS = [
 export const FOOTER_LINKS = [
   { iconName: "email", href: "mailto:hi@fneira.dev" },
   { iconName: "github", href: "https://github.com/fneiraj" },
+  {
+    iconName: "stackoverflow",
+    href: "https://stackoverflow.com/users/12276050/fneira",
+  },
   { iconName: "linkedin", href: "https://www.linkedin.com/in/fneiraj" },
 ];
 
 export const GISCUS = {
-  id: "comments",
-  repo: "fneiraj/blog",
-  repoId: "R_kgDOIVcPIg",
-  category: "General",
-  categoryId: "DIC_kwDOIVcPIs4Cc8s7",
-  mapping: "pathname",
-  reactionsEnabled: "1",
-  emitMetadata: "0",
-  inputPosition: "bottom",
-  loading: "lazy",
+  src: "https://giscus.app/client.js",
+  "data-repo": "fneiraj/blog",
+  "data-repo-id": "R_kgDOIVcPIg",
+  "data-category": "General",
+  "data-category-id": "DIC_kwDOIVcPIs4Cc8s7",
+  "data-mapping": "pathname",
+  "data-strict": "0",
+  "data-reactions-enabled": "1",
+  "data-emit-metadata": "0",
+  "data-input-position": "bottom",
+  "data-theme": "preferred_color_scheme",
+  "data-lang": "es",
 };
