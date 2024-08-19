37ce6be724c2b9c156787d3b9dbced19fd47b052
Fernando Neira <fe.neiraj@gmail.com>
2024-08-19 18:29:19 -0400

    feat: update functions

--- functions/api/posts/[[id]].js
+++ functions/api/posts/[[id]].js
@@ -1,11 +1,45 @@
-export function onRequestGet(context) {
-  const id = context.params.id;
+export const onRequestPut = async (context) => {
+  const {
+    request,
+    env: { blog },
+  } = context;
 
-  if (!id) {
-    return new Response("Not found", { status: 404 });
+  // CF way to access the body of the put request
+  const { path } = await request.json();
+
+  let currentCount = Number(await blog.get(path));
+  if (!currentCount || isNaN(currentCount)) {
+    currentCount = 0;
   }
 
-  return Response.json({
-    views: Math.floor(Math.random() * 100),
+  // KV store does not allows "Number" as value
+  await blog.put(path, String(currentCount + 1));
+
+  return new Response(null, {
+    status: 204,
+    statusText: "ok",
   });
-}
+};
+
+// GET /api/pageViews
+export const onRequestGet = async (context) => {
+  const {
+    request,
+    env: { blog },
+  } = context;
+
+  const path = new URL(request.url).searchParams.get("path");
+
+  if (!path) {
+    return new Response(null, {
+      status: 404,
+    });
+  }
+
+  const encodedPath = encodeURIComponent(path);
+  const count = (await blog.get(encodedPath)) ?? 0;
+
+  return new Response(JSON.stringify({ count }), {
+    status: 200,
+  });
+};
